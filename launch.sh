ee0721722d40c956e189ee69d59faba94f736ebf
David Huveau <d.huveau@gmail.com>
2019-01-28 18:48:08 +0100

    add the animations, music

---
+++ launch.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo 'web server listen 127.0.0.1:8000'
+
+#start a server web  to run the game
+python -m SimpleHTTPServer
+
--- main.js
+++ main.js
@@ -7,6 +7,7 @@ const mainState = {
     // Load the bird sprite
     game.load.image("bird", "assets/bird.png");
     game.load.image("pipe", "assets/pipe.png");
+    game.load.audio("jump", "assets/jump.wav");
   },
 
   create: function() {
@@ -29,6 +30,10 @@ const mainState = {
     // Add gravity to the bird to make it fall
     this.bird.body.gravity.y = 1000;
 
+    // Move the anchor to the left and downward
+    // for the rotation animation
+    this.bird.anchor.setTo(-0.2, 0.5);
+
     // Call the 'jump' function when the spacekey is hit
     const spaceKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
     spaceKey.onDown.add(this.jump, this);
@@ -38,6 +43,8 @@ const mainState = {
 
     // Call the addRowOfPipes() function every 1.5 seconds.
     this.timer = game.time.events.loop(1500, this.addRowOfPipes, this);
+
+    this.jumpSound = game.add.audio("jump");
 
     this.score = 0;
     this.labelScore = game.add.text(20, 20, "0", {
@@ -54,19 +61,41 @@ const mainState = {
     // Call the 'restartGame' function
     if (this.bird.y < 0 || this.bird.y > 490) this.restartGame();
 
+    // game.physics.arcade.overlap(
+    //   this.bird,
+    //   this.pipes,
+    //   this.restartGame,
+    //   null,
+    //   this
+    // );
     game.physics.arcade.overlap(
       this.bird,
       this.pipes,
-      this.restartGame,
+      this.hitPipe,
       null,
       this
     );
+
+    if (this.bird.angle < 20) this.bird.angle += 1;
   },
 
   // Make the bird jump
   jump: function() {
+    if (this.bird.alive == false) return;
+
     // Add a vertical velocity to the bird
     this.bird.body.velocity.y = -350;
+
+    // Create an animation on the bird
+    var animation = game.add.tween(this.bird);
+
+    // Change the angle of the bird to -20° in 100 milliseconds
+    animation.to({ angle: -20 }, 100);
+
+    // And start the animation
+    animation.start();
+
+    this.jumpSound.play();
   },
 
   // Restart the game
@@ -107,6 +136,23 @@ const mainState = {
 
     this.score += 1;
     this.labelScore.text = this.score;
+  },
+
+  hitPipe: function() {
+    // If the bird has already hit a pipe, do nothing
+    // It means the bird is already falling off the screen
+    if (this.bird.alive == false) return;
+
+    // Set the alive property of the bird to false
+    this.bird.alive = false;
+
+    // Prevent new pipes from appearing
+    game.time.events.remove(this.timer);
+
+    // Go through all the pipes, and stop their movement
+    this.pipes.forEach(function(p) {
+      p.body.velocity.x = 0;
+    }, this);
   }
 };
 
